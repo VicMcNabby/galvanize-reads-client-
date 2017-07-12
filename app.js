f54233ecae77b2f06f45dd2b3aa7fbe60a2c8cd1
Matthew Victor <vicmcnabby@gmail.com>
2017-07-11 20:20:48 -0600

    updated books.html

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-// $(appReady);
-//
-// let API_URL = ''
-//
-// function appReady() {
-//   getAllBooks();
-//   showAllBooks();
-// }
-//
-// function getAllBooks() {
-//   return $.get(`${API_URL}/books`);
-// }
-//
-// function showAllBooks() {
-//   let context = {
-//     book_title: books.title,
-//
-//   }
-//   const source = $("#books-template").html();
-//   const template = Handlebars.compile(source);
-//   const html = template(context);
-//   $('main').append(html);
-// }
+$(appReady);
+
+let API_URL = "https://galvanize-reads-api.herokuapp.com"
+
+function appReady() {
+  getAllBooks()
+    .then(showAllBooks);
+}
+
+function getAllBooks() {
+  return $.get(`${API_URL}/books`);
+}
+
+function showAllBooks(books) {
+  const source = $("#books-template").html();
+  const template = Handlebars.compile(source);
+  const html = template({
+    books
+  });
+  $('main').append(html);
+}
