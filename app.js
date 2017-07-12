028bd61e80868faec1d8e039a4bd2f1723ca85a2
Matthew Victor <vicmcnabby@gmail.com>
2017-07-12 11:53:22 -0600

    added one book view

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
+// const API_URL = "https://galvanize-reads-api.herokuapp.com"
+
 $(appReady);
 
 let API_URL = "https://galvanize-reads-api.herokuapp.com"
 
 function appReady() {
+  let id = getID()
   getAllBooks()
-    .then(showAllBooks);
+    .then(showBooks)
+  getOneBook(id)
+    .then(showOneBook)
 }
 
 function getAllBooks() {
   return $.get(`${API_URL}/books`);
 }
 
-function showAllBooks(books) {
+function getOneBook(id) {
+  return $.get(`${API_URL}/books/${id}`)
+}
+
+function showBooks(books) {
   const source = $("#books-template").html();
   const template = Handlebars.compile(source);
   const html = template({
     books
   });
-  $('main').append(html);
+  $('.bookDisplay').append(html);
+}
+
+function showOneBook(books) {
+  $('.bookDisplay').hide()
+  const source = $("#books-template").html();
+  const template = Handlebars.compile(source);
+  const html = template({
+    books
+  });
+  $('.oneBookDisplay').append(html);
+}
+
+function getID() {
+  let currentURL = window.location.href;
+  let charArray = currentURL.split('');
+  let index = charArray.indexOf('=') + 1;
+  let id = currentURL.substring(index);
+  return id;
 }
